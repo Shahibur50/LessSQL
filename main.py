222934b0d32ff8526e0821a78d969d62c2e48c5b
Shahibur50 <72332158+Shahibur50@users.noreply.github.com>
2020-11-11 20:04:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ try:
     connection = True
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
+        print("Something is wrong with your user name or password!")
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+        print("Database does not exist!")
     else:
         print(err)
 
