64e8f7cfabf9cb5f23b753d01cd8e063699c0e5a
Shahibur50 <72332158+Shahibur50@users.noreply.github.com>
2020-11-13 11:03:50 +0530

    Added new functions and documentation updates.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 STUDENT DATABASE MANAGEMENT SYSTEM (SDBMS)
 
-version 1.7.6 (Beta)
+version 1.8.6 (Beta)
 
 Copyright (C) 2020  Shahibur Rahaman
 
@@ -9,13 +9,12 @@ Licensed under GNU GPLv3
 """
 from mysql.connector import connection
 from mysql.connector import errorcode
-from prettytable import from_db_cursor
 from prettytable import PrettyTable
+from prettytable import from_db_cursor
 from datetime import datetime
 import time
 import mysql.connector
 import getpass
-
 
 PT = PrettyTable()
 connection = False
@@ -79,84 +78,107 @@ def main():
     cnx.close()
 
 
-def insert():
-    try:
-        table_name = input("TABLE NAME: ")
-        column = input("COLUMN NAMES: ")
-        values = input("VALUES: ")
-        command = f"INSERT INTO {table_name} ({column}) VALUES ({values})"
-        cursor.execute(command)
-        cnx.commit()
-        print("")
-    except mysql.connector.errors.ProgrammingError:
-        print("Syntax Error!")
-    except mysql.connector.errors.DataError:
-        print("Column count doesn't match value count")
+def show_tb():
+    command = f"SHOW TABLES"
+    cursor.execute(command)
+    Table = from_db_cursor(cursor)
+    Table.align = "l"
+    print(Table)
+    print("")
 
 
-def create():
+def create_tb():
     try:
-        table_name = input("NEW TABLE NAME: ")
-        no_of_columns = int(input("NO. OF COLUMNS: "))
+        table_name = input("       -> NAME OF TABLE TO BE CREATED: ")
+        no_of_columns = int(input("      -> NO. OF COLUMNS: "))
         columns = ""
         for _ in range(no_of_columns - 1):
-            column_value_type = input("COLUMN NAME AND VALUE-TYPE: ")
+            column_value_type = input("       -> COLUMN NAME AND VALUE-TYPE: ")
             columns += column_value_type + ', '
 
-        column_value_type = input("COLUMN NAME AND VALUE-TYPE: ")
+        column_value_type = input("       -> COLUMN NAME AND VALUE-TYPE: ")
         columns += column_value_type
 
         print(columns)
         command = f"CREATE TABLE {table_name}({columns})"
         cursor.execute(command)
         cnx.commit()
+
+        print(f"\nQuery OK, Created the '{table_name}' table.\n")
     except ValueError:
-        print("Please insert values properly!")
+        print("\nERROR! Please insert values properly!\n")
     except mysql.connector.errors.ProgrammingError:
-        print("You have an error in your syntax!")
-    finally:
+        print("\nERROR! You have an error in your syntax!\n")
+
+
+def delete_tb():
+    try:
+        table_name = input("      -> NAME OF TABLE TO BE DELETED: ")
+        
+        command = f"DROP TABLE {table_name}"
+        cursor.execute(command)
+        cnx.commit()
+
+        print(f"\nQuery OK, deleted the table ({table_name})\n")
+    except mysql.connector.errors.ProgrammingError:
+        print("\nERROR! Table not found!\n")
+
+
+def insert():
+    try:
+        table_name = input("       -> TABLE NAME: ")
+        column = input("       -> COLUMN NAMES: ")
+        values = input("       -> VALUES: ")
+        
+        command = f"INSERT INTO {table_name} ({column}) VALUES ({values})"
+        cursor.execute(command)
+        cnx.commit()
         print("")
+    
+    except mysql.connector.errors.ProgrammingError:
+        print("Syntax Error!")
+    except mysql.connector.errors.DataError:
+        print("Column count doesn't match value count")
 
 
 def reveal():
     try:
-        table_name = input("TABLE NAME: ")
+        table_name = input("       -> TABLE NAME: ")
         cursor.execute(f"SELECT * FROM {table_name}")
+        
         Table = from_db_cursor(cursor)
         Table.align = "l"
         print(Table)
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    finally:
-        print("")
+    
+    except mysql.connector.errors.ProgrammingError:
+        print(f"ERROR! Table not found!")
 
 
 def update():
     try:
-        table_name = input("TABLE NAME: ")
-        column_name = input("COLUMN/FIELD TO BE CHECKED: ")
-        value = input("VALUE OF DATA-ITEM TO BE CHECKED: ")
-        attribute = input("COLUMN/FIELD TO BE UPDATED: ")
-        updt_value = input("VALUE OF DATA-ITEM TO BE UPDATED: ")
+        table_name = input("       -> TABLE NAME: ")
+        column_name = input("       -> COLUMN/FIELD TO BE CHECKED: ")
+        value = input("       -> VALUE OF DATA-ITEM TO BE CHECKED: ")
+        attribute = input("       -> COLUMN/FIELD TO BE UPDATED: ")
+        updt_value = input("       -> VALUE OF DATA-ITEM TO BE UPDATED: ")
 
         if "NULL" in value:
             command = f"UPDATE {table_name} SET {attribute}={updt_value} WHERE {column_name} IS {value}"
         else:
-            command = f"UPDATE {table_name} SET {attribute}={updt_value} WHERE {column_name}={value}"
-        
+            command = f"UPDATE {table_name} SET {attribute}={updt_value} WHERE {column_name}={value}" 
         cursor.execute(command)
         cnx.commit()
-        print(f"\nQuery OK, updated the row(s)/record(s) in column/field ({attribute}) to {updt_value} where data-item in ({column_name}) = {value}.")
+        print(f"\nQuery OK, updated the row(s)/record(s) in column/field ({attribute}) to {updt_value} where data-item in ({column_name}) = {value}.\n")
+    
     except mysql.connector.errors.ProgrammingError:
-        print("Data not found!")
-    finally:
-        print("")
+        print("\nData not found!\n")
+
 
 
 def search():
     try:
-        table_name = input("TABLE NAME: ")
-        column_name = input("COLUMN NAME: ")
+        table_name = input("       -> TABLE NAME: ")
+        column_name = input("       -> COLUMN NAME: ")
         value = input("VALUE: ")
 
         if value == "NULL":
@@ -167,6 +189,7 @@ def search():
         Table = from_db_cursor(cursor)
         Table.align = "l"
         print(Table)
+    
     except mysql.connector.errors.ProgrammingError:
         print("Data not found!")
     finally:
@@ -176,18 +199,18 @@ def search():
 
 def delete():
     try:
-        table_name = input("TABLE NAME: ")
-        column_name = input("COLUMN/FIELD NAME: ")
-        value = input("DATA-ITEM VALUE: ")
+        table_name = input("       -> TABLE NAME: ")
+        column_name = input("       -> COLUMN/FIELD NAME: ")
+        value = input("       -> DATA-ITEM VALUE: ")
 
         if "NULL" in value:
             command = f"DELETE FROM {table_name} WHERE {column_name} IS {value}"
         else:
-            command = f"DELETE FROM {table_name} WHERE {column_name}={value}"
-        
+            command = f"DELETE FROM {table_name} WHERE {column_name}={value}" 
         cursor.execute(command)
         cnx.commit()
         print(f"Query OK, deleted the row(s)/record(s) containing the value {value}.")
+    
     except mysql.connector.errors.ProgrammingError:
         print("Data not found!")
     finally:
@@ -196,22 +219,42 @@ def delete():
 
 def instructions():
     print("""
-Usage instructions:
+                                  INSTRUCTIONS
+                                  ------------
 
-insert() : To insert data into a specific table.
-create() : To create a new table.
-delete() : To delete a row.
-reveal() : To show all of the data stored in the specific table.
-search() : To search for a particular row in a table.
-update() : To modify or change value of a data-item present in a coulmn/field.
-quit()   : To quit the program.
+COMMANDS FOR SOFTWARE INFO:
+
+show_w() > Warranty info for this software.
+show_c() > Terms and conditions for redistribution of this software
+show_v() > To show info related to this software.
+______________________________________________________________________________
+
+COMMANDS FOR IN-TABLE QUERIES AND MANIPULATION:
+
+insert() > To insert data into a specific table.
+delete() > To delete a row.
+reveal() > To show all of the data stored in the specific table.
+search() > To search for a particular row in a table.
+update() > To modify or change value of a data-item present in a coulmn/field.
+______________________________________________________________________________
+
+COMMANDS TO SHOW, CREATE AND DELETE TABLES:
+
+show_tb()   > To show tables present in a database.
+create_tb() > To create a new table.
+delete_tb() > To delete a table completely.
+______________________________________________________________________________
+
+COMMAND TO EXIT THE PROGRAM:
+
+quit() > To quit the program.
 """)
 
 
 def to_user():
     print(f"""
 WELCOME TO SCHOOL DATABASE MANAGEMENT SYSTEM (SDBMS)
-Version: 1.7.6 (Beta)
+Version: 1.8.6 (Beta)
 
 Copyright (C) 2020 Shahibur Rahaman
 
@@ -224,7 +267,7 @@ You are connected to'{db}' database
 
 Commands end with ()
 
-For program info type 'info()'; For help type 'help()'
+For program info type 'version()'; For help type 'help()'
 """)
 
 
@@ -277,22 +320,16 @@ your copyrighted material outside their relationship with you.
 For license info visit: https://www.gnu.org/licenses/gpl-3.0.html
 """)
 
-def version():
+def show_v():
     print("""
-VERSION: 1.7.6 (Beta)
-
+VERSION: 1.8.6 (Beta)
 SCHOOL DATABASE MANAGEMENT SYSTEM (SDBMS)
 
 Copyright (C) 2020  Shahibur Rahaman
 License GPLv3+: GNU GPL version 3 or later
 
-This software is a database management system based on MySQL, with a goal
+This program is a database management system based on MySQL, with a goal
 to simplify the usage of a RDBMS for record keeping of students in schools.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
