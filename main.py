fda2ee9f750faed3b1fb17c2f38014b36585fec0
Shahibur Rahaman <72332158+Shahibur50@users.noreply.github.com>
2021-02-17 22:05:50 +0530

    Change License: MIT -> GNU GPLv3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """
 LessSQL
-Version: 4.2.13
+Version: 4.3.13
 
 Copyright (c) 2021 Shahibur Rahaman
-Licensed under MIT
+Licensed under GNU GPLv3
 """
 
 import sys
@@ -1310,7 +1310,7 @@ def to_user():
     print(r"""
 +------------------------------------------------------------+
 | Welcome to LessSQL Database Management Client              |
-| Version: 4.2.13                                            |
+| Version: 4.3.13                                            |
 |                                                            |
 | Copyright (c) 2021 Shahibur Rahaman                        |
 |                                                            |
@@ -1328,29 +1328,8 @@ def to_user():
 
 
 def lesssql_license():
-    print("""
-MIT License
-
-Copyright (c) 2020 Shahibur Rahaman
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-""")
+    main_license = open("LICENSE.txt", "r").read()
+    print(main_license)
 
 
 if __name__ == "__main__":
